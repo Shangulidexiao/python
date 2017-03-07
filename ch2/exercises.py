b75f448121f964d33b2683db8efc3263745bfe36
liliting <18335832714@163.com>
2017-03-08 00:09:01 +0800

    ch2

--- ch2/exercises.py
+++ ch2/exercises.py
@@ -18,4 +18,83 @@ print 'My name is %s , age is %d' % (name,age)
 # (e) 在 1 + 2 * 4 的前面加入print
 
 # 2.3 
+8 + 4
+9 - 4
+8 / 4
+4 * 5
+9 % 5
+5 ** 2
 
+
+# 2.4
+str1 = raw_input('please input you like string:')
+print str1
+
+num = raw_input('please input a number:')
+print int(num)
+
+# 2.5
+
+i = 0
+while i <= 10:
+	print i
+	i += 1
+
+for item in range(11):
+	print item
+
+# 2.6
+num = int(raw_input('please input a number:'))
+if num > 0:
+	print '正数'
+elif num == 0:
+	print '等于0'
+else:
+	print '负数'
+
+# 2.7
+str2 = raw_input('please input a string:\n')
+str2Len = len(str2)
+i = 0
+while i < str2Len:
+	print str2[i]
+	i += 1
+
+for s in range(str2Len):
+	print str2[s]
+
+# 2.8
+# 自定义列表
+list1 = [1,2,3,4,5]
+print sum(list1)
+
+# for循环列表
+list2 = []
+for i in range(5):
+	num = int(raw_input('please input a number:'))
+	list2.append(num)
+print sum(list2)
+
+# while循环列表
+list3 = []
+i = 0
+while i < 5:
+	num = int(raw_input('please input a number:'))
+	list3.append(num)
+	i += 1
+print sum(list3)
+
+# 2.9 
+
+flist = [2,3,4,5,7]
+print float(sum(flist))/len(flist)
+
+# 2.10
+
+while True:
+	num = int(raw_input('Please input a 0~100 number:\n'))
+	if 0 <= num <= 100:
+		print 'ok'
+		break
+	else:
+		print 'you input is error'
